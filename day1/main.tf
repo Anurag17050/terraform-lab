eb2be5286864c4951761b1e5cbc6b3861f4630d9
Anurag17050 <mardaanurag209@gmail.com>
2026-03-18 10:12:58 +0000

    test fmt failure

--- day1/main.tf
+++ day1/main.tf
@@ -2,9 +2,9 @@ variable "message" {
   default = "Hello from Terraform"
 }
 
-resource "local_file" "hello" {
-  filename = "hello.txt"
-  content  = var.message
+resource "local_file" "hello"{
+filename="test.txt"
+content="Hello"
 }
 output "file_path" {
   value = local_file.hello.filename
