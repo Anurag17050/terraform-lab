7769bec8f6abcf059e99e3884a6f22180a203dd5
Anurag17050 <mardaanurag209@gmail.com>
2026-03-18 10:16:40 +0000

    test validate failure

--- day1/main.tf
+++ day1/main.tf
@@ -2,10 +2,9 @@ variable "message" {
   default = "Hello from Terraform"
 }
 
-resource "local_file" "hello"{
-filename="test.txt"
-content="Hello"
-}
+resource "local_file" "hello" {
+filename = "test.txt"
+content = "Hello"
 output "file_path" {
   value = local_file.hello.filename
 }
